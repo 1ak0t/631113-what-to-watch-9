ff3568f355b8b757bb74a9edd1838d5d85c356b4
Кот Игорь Александрович <kotia@irbis.local>
2022-02-16 09:30:46 +0700

    Lesson 2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import StartScreen from '../start-screen/start-screen';
+
+type AppScreenProps = {
+  filmsCount: number;
+  promoFilmName: string;
+  promoFilmGenre: string;
+  promoFilmYear: number;
+}
+
+function App({filmsCount, promoFilmName, promoFilmGenre, promoFilmYear}: AppScreenProps): JSX.Element {
+  return (
+    <StartScreen
+      filmsCount={filmsCount}
+      promoFilmName={promoFilmName}
+      promoFilmGenre={promoFilmGenre}
+      promoFilmYear={promoFilmYear}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Settings = {
+  FILMS_ON_PAGE: 20,
+  PROMO_FILM_NAME: 'The Grand Budapest Hotel',
+  PROMO_FILM_GENRE: 'Drama',
+  PROMO_FILM_YEAR: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmsCount = {Settings.FILMS_ON_PAGE}
+      promoFilmName={Settings.PROMO_FILM_NAME}
+      promoFilmGenre={Settings.PROMO_FILM_GENRE}
+      promoFilmYear={Settings.PROMO_FILM_YEAR}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
