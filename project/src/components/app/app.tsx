de284596393b926191a5538026a539ecea9bc052
Кот Игорь Александрович <kotia@irbis.local>
2022-03-14 17:09:02 +0700

    Lesson 3

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import StartScreen from '../start-screen/start-screen';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import NotFound from '../not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   filmsCount: number;
@@ -9,12 +18,49 @@ type AppScreenProps = {
 
 function App({filmsCount, promoFilmName, promoFilmGenre, promoFilmYear}: AppScreenProps): JSX.Element {
   return (
-    <StartScreen
-      filmsCount={filmsCount}
-      promoFilmName={promoFilmName}
-      promoFilmGenre={promoFilmGenre}
-      promoFilmYear={promoFilmYear}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <StartScreen
+              filmsCount={filmsCount}
+              promoFilmName={promoFilmName}
+              promoFilmGenre={promoFilmGenre}
+              promoFilmYear={promoFilmYear}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePage />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/not-found/not-found.tsx
@@ -0,0 +1,43 @@
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
+
+function NotFound(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">404</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <h2 className="film-card__title" style={{textAlign: 'center'}}>Page Not Found</h2>
+        <div className="sign-in__submit">
+          <Link to={AppRoute.SignIn} className="sign-in__btn" type="submit">Sign in</Link>
+        </div>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFound;
