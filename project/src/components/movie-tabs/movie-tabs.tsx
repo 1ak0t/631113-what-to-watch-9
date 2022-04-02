0c3e10b5dc698bcba1dfc1bb59cda242cec45bc0
Кот Игорь Александрович <kotia@irbis.local>
2022-04-02 12:45:23 +0700

    Lesson 5.2 fixed

--- project/src/components/movie-tabs/movie-tabs.tsx
+++ project/src/components/movie-tabs/movie-tabs.tsx
@@ -1,5 +1,5 @@
 import {MovieTabsName} from '../../const';
-import {useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 import React from 'react';
 import MovieOverview from '../movie-overview/movie-overview';
 import MovieDetails from '../movie-details/movie-details';
@@ -19,15 +19,17 @@ function MovieTabs({reviews, film}: MovieTabsProps) {
     setActiveTab(evt.target.textContent);
   };
 
-  const overviewTabRef = useRef<HTMLLIElement | null>(null);
-  const detailsTabRef = useRef<HTMLLIElement | null>(null);
-  const reviewsTabRef = useRef<HTMLLIElement | null>(null);
+  const tabNames = Object.values(MovieTabsName);
 
-  const removeActiveClass = (tab: React.MutableRefObject<HTMLLIElement | null>) => {
-    if (tab.current !== null) {
-      tab.current.classList.remove('film-nav__item--active');
-    }
+  const getMovieTabs = (name: string, clickHandler: (evt: any) => void) => {
+    const activeClass = name === activeTab ? 'film-nav__item--active' : '';
+    return (
+      <li className={`film-nav__item ${activeClass}`} onClick={clickHandler}>
+        <a className="film-nav__link">{name}</a>
+      </li>
+    );
   };
+
 
   const chooseTabInfo = (tab: string) => {
     switch (tab) {
@@ -40,42 +42,11 @@ function MovieTabs({reviews, film}: MovieTabsProps) {
     }
   };
 
-  useEffect(() => {
-    switch (activeTab) {
-      case MovieTabsName.Overview:
-        if (overviewTabRef.current !== null){
-          overviewTabRef.current.classList.add('film-nav__item--active');
-        }
-        break;
-      case MovieTabsName.Details:
-        if (detailsTabRef.current !== null){
-          detailsTabRef.current.classList.add('film-nav__item--active');
-        }
-        break;
-      case MovieTabsName.Reviews:
-        if (reviewsTabRef.current !== null){
-          reviewsTabRef.current.classList.add('film-nav__item--active');
-        }
-    } return (() => {
-      removeActiveClass(overviewTabRef);
-      removeActiveClass(detailsTabRef);
-      removeActiveClass(reviewsTabRef);
-    });
-  }, [activeTab]);
-
   return (
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li ref={overviewTabRef} className="film-nav__item" onClick={tabClickHandler}>
-            <a className="film-nav__link">{MovieTabsName.Overview}</a>
-          </li>
-          <li ref={detailsTabRef} className="film-nav__item" onClick={tabClickHandler}>
-            <a className="film-nav__link">{MovieTabsName.Details}</a>
-          </li>
-          <li ref={reviewsTabRef} className="film-nav__item" onClick={tabClickHandler}>
-            <a className="film-nav__link">{MovieTabsName.Reviews}</a>
-          </li>
+          {tabNames.map((tabName) => getMovieTabs(tabName, tabClickHandler))}
         </ul>
       </nav>
       {chooseTabInfo(activeTab)}
